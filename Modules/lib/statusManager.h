1b3f203ae7278c55447d56d378a05d918fc00078
Mapy542 <elimbukoski@gmail.com>
2024-07-25 23:53:54 -0400

    add chain callback

--- Modules/lib/statusManager.h
+++ Modules/lib/statusManager.h
@@ -4,22 +4,27 @@
 #include <stdint.h>
 
 namespace statusManager {
-class statusManager {
-   private:
-    uint8_t systemStatus;  // The status of the system
+    class statusManager {
+    private:
+        uint8_t systemStatus;  // The status of the system
 
-   public:
-    statusManager();  // Constructor
+    public:
+        statusManager();  // Constructor
 
-    uint8_t getSystemStatus();  // Get the status of the system
-    bool getOperable();         // Get whether the system is operable
+        uint8_t getSystemStatus();  // Get the status of the system
+        bool getOperable();         // Get whether the system is operable
 
-    void ConfiguredCallback();  // Callback for when the system is configured, call when the module
-                                // receives any configuration
+        void ConfiguredCallback();  // Callback for when the system is configured, call when the module
+        // receives any configuration
 
-    void ErrorCallback(bool inoperable);  // Callback for when the system encounters an error, call
-                                          // when the module encounters an error.
-};
+        void ErrorCallback(bool inoperable);  // Callback for when the system encounters an error, call
+        // when the module encounters an error.
+
+        void chainNeighborCallback(
+            bool neighborAcquired,
+            bool chainFunctional);  // Callback from the chain manager to indicate if a neighbor has
+        // been acquired and if the chain is functional
+    };
 };  // namespace statusManager
 
 #endif

c34f0a1a3b679a770e7d3c5d24a0037745a16360
Mapy542 <elimbukoski@gmail.com>
2024-08-16 20:41:47 -0400

    add private functions

--- SBC/lib/transportAgent.cpp
+++ SBC/lib/transportAgent.cpp
@@ -1,5 +1,49 @@
 #include "transportAgent.h"
 
+/*---- Private Functions ----*/
+void TransportAgent::transportAgentWorker() {
+    // The worker function for the transport agent thread
+    while (true) {
+        // Send all general packets
+    }
+}
+
+uint32_t TransportAgent::generateGeneralPacketUID(ROIPackets::Packet packet) {
+    uint32_t uid = 0;
+    uid +=
+        packet
+            .getHostAddressOctet();  // make these interchangable as they will be swapped on receive
+    uid += packet.getClientAddressOctet();
+
+    uid *= 17;  // Prime number to spread out the values
+
+    uid += packet.getSubDeviceID();  // this stays the same, so doesn't need to be interchangable
+    uid *= 11;                       // Prime number
+    uid += packet.getActionCode();
+    uid *= 13;  // Prime number
+
+    return uid;
+}
+
+uint32_t TransportAgent::generateSysAdminPacketUID(ROIPackets::sysAdminPacket packet) {
+    uint32_t uid = 0;
+    uid +=
+        packet
+            .getHostAddressOctet();  // make these interchangable as they will be swapped on receive
+    uid += packet.getClientAddressOctet();
+
+    uid *= 17;  // Prime number to spread out the values
+
+    uid += packet.getOriginOctet();  // this stays the same, so doesn't need to be interchangable
+    uid *= 11;                       // Prime number
+    uid += packet.getActionCode();
+    uid *= 13;  // Prime number
+
+    return uid;
+}
+
+/*---- Public Functions ----*/
+
 TransportAgent::TransportAgent() {}
 
 TransportAgent::~TransportAgent() {
@@ -37,11 +81,4 @@ void TransportAgent::queueGeneralPacket(ROIPackets::Packet packet) {
 void TransportAgent::queueSysAdminPacket(ROIPackets::sysAdminPacket packet) {
     // Queue a sysAdmin packet to be sent to the modules
     sysAdminPacketQueue.push_back(packet);
-}
-
-void TransportAgent::transportAgentWorker() {
-    // The worker function for the transport agent thread
-    while (true) {
-        // Send all general packets
-    }
 }

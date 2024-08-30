3b56fe879a0c58e1e55d5dd550e7ec2774872fc9
Mapy542 <elimbukoski@gmail.com>
2024-08-30 09:35:51 -0400

    prep base module to be node

--- ROS/libs/moduleVirtualizations/base.h
+++ ROS/libs/moduleVirtualizations/base.h
@@ -6,8 +6,12 @@
 #include "../../../lib/ModuleCodec.h"
 #include "../../../lib/Packet.h"
 #include "../../../lib/UnityTypes.hpp"
-#include "../transportAgent.h"
-
+#include "../socketwrapper-2/include/socketwrapper/endpoint.hpp"
+#include "../socketwrapper-2/include/socketwrapper/socketwrapper.hpp"
+#include "../socketwrapper-2/include/socketwrapper/span.hpp"
+#include "../socketwrapper-2/include/socketwrapper/udp.hpp"
+#include "../socketwrapper-2/include/socketwrapper/utility.hpp"
+#include "rclcpp/rclcpp.hpp"
 /*
 
 This is the base module template* for all virtual modules (See virtualization layer). It defines
@@ -41,6 +45,25 @@ class BaseModule {
 
     friend class TransportAgent;  // TransportAgent needs access to the
                                   // ResponseCallback and MaintainState functions
+};
+
+class ROINode : public rclcpp::Node {
+   public:
+    ROINode() : Node("roi_integration") {
+        publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+        timer_ = this->create_wall_timer(500ms, std::bind(&MinimalPublisher::timer_callback, this));
+    }
+
+   private:
+    void timer_callback() {
+        auto message = std_msgs::msg::String();
+        message.data = "Hello, world! " + std::to_string(count_++);
+        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+        publisher_->publish(message);
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    size_t count_;
 };
 
 #endif  // BASEMODULE_H

04946ed172d39f6cf699bc2217196317606864c3
Eli <elimbukoski@gmail.com>
2024-09-15 02:06:11 +0000

    in working condition

--- ROS/src/base.cpp
+++ ROS/src/base.cpp
@@ -2,6 +2,11 @@
 
 void BaseModule::debugLog(std::string message) { RCLCPP_INFO(this->get_logger(), message.c_str()); }
 
-uint8_t BaseModule::getOctet() { return _moduleOctet; }
+uint8_t BaseModule::getOctet() { return this->get_parameter("module_octet").as_int(); }
 
-std::string BaseModule::getAlias() { return _moduleAlias; }
+std::string BaseModule::getAlias() { return this->get_parameter("module_alias").as_string(); }
+
+int main(int argc, char *argv[]) {
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}

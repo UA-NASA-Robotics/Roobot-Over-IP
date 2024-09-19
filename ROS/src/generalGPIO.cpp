d81c8046c2723a4aaa4eb7fbc813291b19c869b5
Eli <elimbukoski@gmail.com>
2024-09-19 23:04:11 +0000

    add generalgpio

--- ROS/src/generalGPIO.cpp
+++ ROS/src/generalGPIO.cpp
@@ -1,5 +1,22 @@
 #include "generalGPIO.h"
 
+//-------- PRIVATE METHODS --------//
+
+//-------- PUBLIC METHODS --------//
+
+GeneralGPIO::GeneralGPIO() : BaseModule() {
+    // Initialize the GPIO module
+    this->debugLog("Initializing General GPIO Module");
+    this->_moduleAliasParameter = this->declare_parameter("module_alias", "blankGeneralGPIO");
+    this->_moduleOctetParameter = this->declare_parameter("module_octet", 5);
+    this->_health_publisher_ = this->create_publisher<roi_ros::msg::Health>("health", 10);
+    this->_queue_general_packet_client_ =
+        this->create_client<roi_ros::srv::QueueSerializedGeneralPacket>("queue_general_packet");
+    this->_response_subscription_ = this->create_subscription<roi_ros::msg::SerializedPacket>(
+        "", 10, std::bind(&GeneralGPIO::responseCallback, this, std::placeholders::_1));
+    this->_maintainStateThread = std::thread(&GeneralGPIO::maintainState, this);
+}
+
 int main(int argc, char *argv[]) {
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<GeneralGPIO>());
--- ROS/src/generalGPIO.h
+++ ROS/src/generalGPIO.h
@@ -3,4 +3,50 @@
 
 #include "base.h"
 
+class generalGPIOModule : public BaseModule {
+   protected:
+    uint8_t subDeviceState[GeneralGPIOConstants::COUNT] = {GeneralGPIOConstants::INPUT_MODE};
+    uint8_t subDeviceMode[GeneralGPIOConstants::COUNT] = {0};
+
+    /**
+     * @brief A callback function for the module to handle octet parameter changes
+     *
+     * @param parameter
+     * @return * rcl_interfaces::msg::SetParametersResult
+     */
+    rcl_interfaces::msg::SetParametersResult octetParameterCallback(
+        const rclcpp::Parameter &parameter);
+
+    /**
+     * @brief A callback function for the module to handle alias parameter changes
+     *
+     * @param parameter
+     * @return * rcl_interfaces::msg::SetParametersResult
+     */
+    rcl_interfaces::msg::SetParametersResult aliasParameterCallback(
+        const rclcpp::Parameter &parameter);
+
+    /**
+     * @brief A worker function for the module to maintain its state, in a separate thread
+     *  For the general GPIO module, this function will be used to refresh input values.
+     *  It will also push the current state to the physical module if the module is reset.
+     */
+
+    void maintainState();
+
+    /**
+     * @brief Callback for the response from the transport agent when a response is received
+     *
+     * @param response , roi_ros::msg::SerializedPacket, the response packet
+     */
+    void responseCallback(const roi_ros::msg::SerializedPacket response);
+
+   public:
+    generalGPIOModule();
+    ~generalGPIOModule();
+
+    bool pushState();
+    bool pullState();
+};
+
 #endif

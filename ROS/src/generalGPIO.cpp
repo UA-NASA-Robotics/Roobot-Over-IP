fe9b0e5381104f8ffa724822bb4e1bc97b74dc2d
Eli <elimbukoski@gmail.com>
2024-09-21 18:00:31 +0000

    add all functions

--- ROS/src/generalGPIO.cpp
+++ ROS/src/generalGPIO.cpp
@@ -2,20 +2,117 @@
 
 //-------- PRIVATE METHODS --------//
 
+rcl_interfaces::msg::SetParametersResult GeneralGPIO::octetParameterCallback(
+    const rclcpp::Parameter &parameter) {
+    // Handle the octet parameter change
+    this->debugLog("Octet parameter changed to " + std::to_string(parameter.as_int()));
+
+    // Unsubscribe from the old response topic
+    this->_response_subscription_.~Subscription();
+    // Subscribe to the new response topic, at the new octet
+    this->_response_subscription_ = this->create_subscription<roi_ros::msg::SerializedPacket>(
+        "octet" + std::to_string(parameter.as_int()) + "_response", 10,
+        std::bind(&GeneralGPIO::responseCallback, this, std::placeholders::_1));
+
+    // Publish a health message mentioning the octet change
+    auto healthMsg = roi_ros::msg::Health();
+    healthMsg.operation_status = 1;
+    healthMsg.message = "Octet parameter changed to " + std::to_string(parameter.as_int());
+    this->_health_publisher_->publish();
+
+    // synchronize with the new module
+    this->pushState();
+
+    this->debugLog("Octet parameter change handled");
+
+    return rcl_interfaces::msg::SetParametersResult();
+}
+
+rcl_interfaces::msg::SetParametersResult GeneralGPIO::aliasParameterCallback(
+    const rclcpp::Parameter &parameter) {
+    // Handle the alias parameter change
+    this->debugLog("Alias parameter changed to " + parameter.as_string());
+
+    // Publish a health message mentioning the alias change
+    auto healthMsg = roi_ros::msg::Health();
+    healthMsg.operation_status = 1;
+    healthMsg.message = "Alias parameter changed to " + parameter.as_string();
+    this->_health_publisher_->publish();
+
+    this->debugLog("Alias parameter change handled");
+
+    return rcl_interfaces::msg::SetParametersResult();
+}
+
+void GeneralGPIO::maintainState() {
+    // Maintain the state of the GPIO module
+    this->debugLog("Maintaining GPIO Module State");
+
+    // Loop to maintain the state
+    uint8_t checkResetCounter = 0;
+    while (rclcpp::ok()) {
+        // Check if the module has been reset
+
+        // Loop through all the subdevices and read the inputs.
+        for (int i = 0; i < GeneralGPIOConstants::COUNT; i++) {
+            if (this->subDeviceState[i] == GeneralGPIOConstants::INPUT_MODE ||
+                this->subDeviceState[i] == GeneralGPIOConstants::INPUT_PULLUP_MODE) {
+                this->sendReadPinPacket(i);
+            }
+        }
+
+        // Sleep for 1 second
+        std::this_thread::sleep_for(
+            std::chrono::milliseconds(moduleNodeConstants::maintainStateSleepTime));
+    }
+}
+
 //-------- PUBLIC METHODS --------//
 
 GeneralGPIO::GeneralGPIO() : BaseModule() {
     // Initialize the GPIO module
     this->debugLog("Initializing General GPIO Module");
+
+    // Initialize the GPIO module parameters and callbacks
     this->_moduleAliasParameter = this->declare_parameter("module_alias", "blankGeneralGPIO");
     this->_moduleOctetParameter = this->declare_parameter("module_octet", 5);
+    this->_octetParameterCallbackHandle = this->add_on_set_parameters_callback(
+        std::bind(&GeneralGPIO::octetParameterCallback, this, std::placeholders::_1));
+    this->_aliasParameterCallbackHandle = this->add_on_set_parameters_callback(
+        std::bind(&GeneralGPIO::aliasParameterCallback, this, std::placeholders::_1));
 
+    // Initialize the GPIO module health publisher
     this->_health_publisher_ = this->create_publisher<roi_ros::msg::Health>("health", 10);
+
+    // Initialize the GPIO module general packet queue client
     this->_queue_general_packet_client_ =
         this->create_client<roi_ros::srv::QueueSerializedGeneralPacket>("queue_general_packet");
+
+    // Initialize the GPIO module response subscription
     this->_response_subscription_ = this->create_subscription<roi_ros::msg::SerializedPacket>(
-        "", 10, std::bind(&GeneralGPIO::responseCallback, this, std::placeholders::_1));
+        "octet5_response", 10,
+        std::bind(&GeneralGPIO::responseCallback, this, std::placeholders::_1));
+
+    // Initialize the GPIO module specific publishers
+    this->_pinStatesPublisher = this->create_publisher<roi_ros::msg::PinStates>("pin_states", 10);
+    this->_pinValuesPublisher = this->create_publisher<roi_ros::msg::PinValues>("pin_values", 10);
+
+    this->_setPinOutputService = this->create_service<roi_ros::srv::SetPinOutput>(
+        "set_pin_output",
+        std::bind(&GeneralGPIO::setPinOutput, this, std::placeholders::_1, std::placeholders::_2));
+    this->_setPinStateService = this->create_service<roi_ros::srv::SetPinState>(
+        "set_pin_state",
+        std::bind(&GeneralGPIO::setPinState, this, std::placeholders::_1, std::placeholders::_2));
+
+    // Initialize the GPIO module maintain state thread
     this->_maintainStateThread = std::thread(&GeneralGPIO::maintainState, this);
+}
+
+GeneralGPIO::~GeneralGPIO() {
+    // Destroy the GPIO module
+    this->debugLog("Destroying General GPIO Module");
+    this->_maintainStateThread
+        .join();  // Wait for the maintain state thread to finish (It should stop itself)
 }
 
 int main(int argc, char *argv[]) {
--- ROS/src/generalGPIO.h
+++ ROS/src/generalGPIO.h
@@ -2,11 +2,23 @@
 #define GENERALGPIO_H
 
 #include "base.h"
+#include "roi_ros/msg/PinStates.msg"
+#include "roi_ros/msg/PinValues.hpp"
+#include "roi_ros/srv/set_pin_output.hpp"
+#include "roi_ros/srv/set_pin_state.hpp"
 
 class generalGPIOModule : public BaseModule {
    protected:
     uint8_t subDeviceState[GeneralGPIOConstants::COUNT] = {GeneralGPIOConstants::INPUT_MODE};
     uint8_t subDeviceMode[GeneralGPIOConstants::COUNT] = {0};
+
+    // Value Topics
+    rclcpp::Publisher<roi_ros::msg::PinStates>::SharedPtr _pinStatesPublisher;
+    rclcpp::Publisher<roi_ros::msg::PinValues>::SharedPtr _pinValuesPublisher;
+
+    // Service Msgs
+    rclcpp::Service<roi_ros::srv::SetPinOutput>::SharedPtr _setPinOutputService;
+    rclcpp::Service<roi_ros::srv::SetPinState>::SharedPtr _setPinStateService;
 
     /**
      * @brief A callback function for the module to handle octet parameter changes
@@ -45,8 +57,49 @@ class generalGPIOModule : public BaseModule {
     generalGPIOModule();
     ~generalGPIOModule();
 
+    /**
+     * @brief Pushes the current state of the GPIO module to the physical module
+     *
+     * @return true, if the state was successfully pushed
+     * @return false, if the state was not successfully pushed
+     */
     bool pushState();
+    /**
+     * @brief WIP, Pulls the current state of the GPIO module from the physical module
+     *
+     * @return true, if the state was successfully pulled
+     * @return false, if the state was not successfully pulled
+     */
     bool pullState();
+
+    /**
+     * @brief Sends a packet changing the state of a pin
+     *
+     * @param pin , uint8_t, the pin to change the state of
+     * @param state , uint8_t, the state to change the pin to
+     * @return true, if the packet was successfully sent
+     * @return false, if the packet was not successfully sent
+     */
+    bool sendSetPinStatePacket(uint8_t pin, uint8_t state);
+
+    /**
+     * @brief Sends a packet changing the output of a pin
+     *
+     * @param pin , uint8_t, the pin to change the output of
+     * @param output , uint8_t, the output to change the pin to
+     * @return true
+     * @return false
+     */
+    bool sendSetPinOutputPacket(uint8_t pin, uint8_t output);
+
+    /**
+     * @brief Sends a packet to read the value of a pin
+     *
+     * @param pin , uint8_t, the pin to read the value of
+     * @return true, if the packet was successfully sent
+     * @return false , if the packet was not successfully sent
+     */
+    bool sendReadPinPacket(uint8_t pin);
 };
 
 #endif

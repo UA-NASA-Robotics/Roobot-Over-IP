39da9a3bbe035fe18829b152d9465650390b32fa
ToxicogenicBees <tookieboy03@icloud.com>
2025-01-30 21:58:03 -0500

    Templatized ActuatorContainer instead of using dynamic memory

--- Modules/Actuator ROI Module/include/ActuatorContainer.h
+++ Modules/Actuator ROI Module/include/ActuatorContainer.h
@@ -1,29 +1,104 @@
-#ifndef ACT_CONT_H
-#define ACT_CONT_H
+#ifndef ACTUATOR_CONTAINER_H
+#define ACTUATOR_CONTAINER_H
 
-#include "../include/Actuator.h"
+#include "Actuator.h"
+#include <stdint.h>
 
+template<int N>
 class ActuatorContainer {
     private:
-        const uint16_t _ENC_LOAD, _ENC_CLK; // Encoder registerload/clock pins
-        const uint8_t _SIZE;                // Container size
-        uint8_t _count;                     // Number of connected actuators
+        bool _initialized;      // Flag to determine if container was initialized
+        const uint8_t _SIZE;    // Container size
 
-        Actuator** _actuators;              // Dynamic array of pointer to Actuator
+        Actuator* _acts[N];     // Array of pointer to actuators
 
-        void _readEncoders();               // Helper function to read the encoder values of all the encoders
-        
     public:
-        ActuatorContainer(uint8_t size, uint8_t encoder_load, uint8_t encoder_clock);
+        /**
+        * @brief ActuatorContainer class constructor
+        */
+        ActuatorContainer();
+
+        /**
+        * @brief ActuatorContainer class deconstructor
+        */
         ~ActuatorContainer();
 
-        Actuator* operator[](uint8_t index);
-        uint8_t getSize();
-        uint8_t getCount();
+        /**
+        * @brief Append an actuator to the end of the container's array
+        * 
+        * @param act    An actuator class object
+        */
+        void append(Actuator& act);
 
-        void connect(Actuator* actuator);
+        /**
+        * @brief ActuatorContainer class constructor
+        * 
+        * @param act    An actuator class object
+        * @param index  The desired index the actuator should be placed
+        */
+        void set(Actuator& act, uint8_t index);
+
+        /**
+        * @brief Initialize each connected actuator
+        */
         void init();
-        void loop();
+
+        /**
+        * @brief Tick each connected actuator
+        */
+        void tick();
 };
+
+
+
+template<int N>
+ActuatorContainer<N>::ActuatorContainer() : _SIZE(N) {
+    _initialized = false;
+    
+    for (int i = 0; i < N; i++)
+        _acts[i] = nullptr;
+}
+
+template<int N>
+ActuatorContainer<N>::~ActuatorContainer() {
+    for (int i = 0; i < N; i++)
+        delete _acts[i];
+}
+
+template<int N>
+void ActuatorContainer<N>::append(Actuator& act) {
+    //assert(_initialized);
+    
+    for (int i = 0; i < N; i++) {
+        if (_acts[N] == nullptr) {
+            _acts[N] = &act;
+            return;
+        }
+    }
+}
+
+template<int N>
+void ActuatorContainer<N>::set(Actuator& act, uint8_t index) {
+    //assert(_initialized && index < N);
+    
+    if (index < _SIZE)
+        _acts[index] = &act;
+}
+
+template<int N>
+void ActuatorContainer<N>::init() {
+    for (Actuator* act : _acts)
+        if (act) act->init();
+    
+    _initialized = true;
+}
+
+template<int N>
+void ActuatorContainer<N>::tick() {
+    for (Actuator* act : _acts) {
+        if (act) act->tick();
+    }
+}
+
 
 #endif

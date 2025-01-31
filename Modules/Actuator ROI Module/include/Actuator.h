cac39d71de8a29922c9b1dd12663c4dcf55a4dbb
ToxicogenicBees <tookieboy03@icloud.com>
2025-01-30 22:04:05 -0500

    Updates to reflect changes

--- Modules/Actuator ROI Module/include/Actuator.h
+++ Modules/Actuator ROI Module/include/Actuator.h
@@ -1,31 +1,56 @@
 #ifndef ACTUATOR_H
 #define ACTUATOR_H
 
-#include "../include/Encoders/EncoderBase.h"
-#include "../include/ActuatorPid.h"
-#include "ActuatorTypes.h"
+#include "MotorDrivers/MotorDriverBase.h"
+#include "Encoders/EncoderDriverBase.h"
+#include "PositionController.h"
+#include "LimitSwitch.h"
 
-// When homing, pass the maximum traversal length as a constructor constant, then home either max/min length
+// Control mode states
+#define VEL_CONTROL true
+#define POS_CONTROL false
+
+// Limit switch states
+#define NO_LIMITS 0
+#define UPPER_LIMIT 1
+#define LOWER_LIMIT 2
+#define BOTH_LIMITS 3
 
 class Actuator {
     private:
-        const uint16_t _TRAVEL_LIMIT;   // The amount of distance the actuator can move between it's
-                                        // minimum and maximum limits
+        bool _initialized;                  // Bool to check if actuator was initialized
+        bool _control_mode;                 // Control mode flag (CMF)
+        const uint8_t _LIMIT_STATE;         // Limit switch state
 
-        ActuatorPins _PINS;             // Actuator's Arduino pins
-        ActuatorPid _pid;               // Actuator's PID controller
-        EncoderBase* _enc;              // External encoder
+        float _velocity;                    // Target velocity (CMF == VEL_CONTROL)
+        uint16_t _position;                 // Target position (CMF == POS_CONTROL)
+
+        MotorDriverBase* _motor;            // Motor driver
+        EncoderDriverBase* _enc;            // Encoder
+        LimitSwitch* _upper_limit;          // Upper limit switch
+        LimitSwitch* _lower_limit;          // Lower limit switch
+
+        PositionController _pos_control;    // Position controller
+
+        bool _limitSwitchActivated();       // Returns if a limit switch is active or not
 
     public:
-        friend class ActuatorContainer;
-        
-        Actuator(ActuatorPins pins = {}, uint16_t travel_limit = 0, PidTuning tuning);
-        Actuator(ActuatorPins pins = {}, uint16_t travel_limit = 0);
-        
-        uint16_t getLastRead();
-        void init();
-        void loop();
-};
+        template<class Encoder, class Motor>
+        Actuator(Encoder* enc, Motor* motor, LimitSwitch* upper, LimitSwitch* lower);
 
+        template<class Encoder, class Motor>
+        Actuator(Encoder* enc, Motor* motor, LimitSwitch* limit_switch, uint8_t limit_state);
+
+        template<class Encoder, class Motor>
+        Actuator(Encoder* enc, Motor* motor);
+
+        void init();
+
+        void tick();
+
+        void targetVelocity(float vel);
+
+        void targetPosition(uint16_t pos);
+};
 
 #endif
--- Modules/Actuator ROI Module/src/Actuator.cpp
+++ Modules/Actuator ROI Module/src/Actuator.cpp
@@ -1,21 +1,115 @@
-#include <Arduino.h>
 #include "../include/Actuator.h"
 
-Actuator::Actuator(ActuatorPins pins = {}, uint16_t travel_limit = 0, PidTuning tuning) :
-_PINS(pins), _TRAVEL_LIMIT(travel_limit) {
-    //_enc = Encoder(_PINS.ENC_CLR, _PINS.ENC_SHFT);
+bool Actuator::_limitSwitchActivated() {
+    switch(_LIMIT_STATE) {
+    case NO_LIMITS:
+        return false;
+    case UPPER_LIMIT:
+        return _upper_limit->state();
+    case LOWER_LIMIT:
+        return _lower_limit->state();
+    case BOTH_LIMITS:
+        return (_upper_limit->state() && _lower_limit->state());
+    }
+}
+
+template<class Encoder, class Motor>
+Actuator::Actuator(Encoder* enc, Motor* motor, LimitSwitch* upper, LimitSwitch* lower) : _LIMIT_STATE(BOTH_LIMITS) {
+    // Initialize flags
+    _control_mode = VEL_CONTROL;
+    _initialized = false;
+
+    // Create / assign child classes
+    _pos_control = PositionController();   // Create a position controller
+
+    // Use provided motor and encoder
+    _motor = motor;
+    _enc = enc;
+
+    // Use provided limit switches
+    _upper_limit = upper;
+    _lower_limit = lower;
+}
+
+template<class Encoder, class Motor>
+Actuator::Actuator(Encoder* enc, Motor* motor, LimitSwitch* limit_switch, uint8_t limit_state) : _LIMIT_STATE(limit_state) {
+    // Initialize flags
+    _control_mode = VEL_CONTROL;
+    _initialized = false;
+
+    // Create / assign child classes
+    _pos_control = PositionController();   // Create a position controller
+
+    // Use provided motor and encoder
+    _motor = motor;
+    _enc = enc;
+
+    // Use provided limit switch
+    if (_LIMIT_STATE == UPPER_LIMIT) {
+        _upper_limit = limit_switch;
+        _lower_limit = nullptr;
+    }
+    else {
+        _upper_limit = nullptr;
+        _lower_limit = limit_switch;
+    }
+}
+
+template<class Encoder, class Motor>
+Actuator::Actuator(Encoder* enc, Motor* motor) : _LIMIT_STATE(NO_LIMITS) {
+    // Initialize flags
+    _control_mode = VEL_CONTROL;
+    _initialized = false;
+
+    // Create / assign child classes
+    _pos_control = PositionController();   // Create a position controller
+
+    // Use provided motor and encoder
+    _motor = motor;
+    _enc = enc;
+
+    // Use provided limit switches or a dummy limit switch
+    _upper_limit = nullptr;
+    _lower_limit = nullptr;
 }
 
 void Actuator::init() {
-    // Initialize the pins on the Arduino
-    pinMode(_PINS.SPEED, OUTPUT);
-    pinMode(_PINS.DIR, OUTPUT);
-    pinMode(_PINS.ENC_CLR, OUTPUT);
-    pinMode(_PINS.ENC_SHFT, OUTPUT);
-    pinMode(_PINS.RET_LMT, INPUT);
-    pinMode(_PINS.EXT_LMT, INPUT);
+    // Init motor and encoder
+    _motor->init();
+    _enc->init();
+
+    // Init limit switches
+    if (_upper_limit) _upper_limit->init();
+    if (_lower_limit) _lower_limit->init();
+
+    // Flag actuator as initialized
+    _initialized = true;
 }
 
-void Actuator::loop() {
-    
+void Actuator::tick() {
+    // Ensure the actuator has been initialized
+    //assert(_initialized);
+
+    // Tick the encoder
+    _enc->tick();
+
+    if (_control_mode == VEL_CONTROL) {
+        // Velocity control
+        _motor->tick();
+    }
+    else {
+        // Position control
+        _pos_control.tick();
+        _motor->tick();
+    }
+}
+
+void Actuator::targetVelocity(float vel) {
+    _control_mode = VEL_CONTROL;
+    _velocity = vel;
+}
+
+void Actuator::targetPosition(uint16_t pos) {
+    _control_mode = POS_CONTROL;
+    _position = pos;
 }

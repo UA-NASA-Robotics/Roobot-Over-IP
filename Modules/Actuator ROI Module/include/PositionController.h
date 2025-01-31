6a11c7dde7e79589b6f22deeee3f34e7349d6e96
ToxicogenicBees <tookieboy03@icloud.com>
2025-01-30 21:56:49 -0500

    New position controller class

---
+++ Modules/Actuator ROI Module/include/PositionController.h
@@ -0,0 +1,14 @@
+#ifndef POSITION_CONTROLLER_H
+#define POSITION_CONTROLLER_H
+
+class PositionController {
+    private:
+
+
+    public:
+        PositionController();
+        
+        void tick();
+};
+
+#endif

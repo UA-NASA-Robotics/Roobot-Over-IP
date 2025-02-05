4c68f8ddd65ff8a9966a4bdc15715899cd8a360c
ToxicogenicBees <tookieboy03@icloud.com>
2025-02-04 22:17:38 -0500

    Started adding Actuator constants

--- lib/ModuleCodec.h
+++ lib/ModuleCodec.h
@@ -269,6 +269,84 @@ constexpr long ODRIVE_ERROR_CALIBRATION_ERROR = 0x40000000;
 
 }  // namespace ODriveConstants
 
+namespace ActuatorConstants {
+namespace MaskConstants {
+constexpr uint16_t SET_MASK = 0x0000;   // Set mask
+constexpr uint16_t GET_MASK = 0x8000;   // Get mask
+constexpr uint8_t LENGTH = 0x00;        // Length operations
+constexpr uint8_t VELOCITY = 0x01;      // Velocity operations
+constexpr uint8_t HOMING = 0x02;        // Homing operations
+constexpr uint8_t CONTROL_FLOW = 0x03;  // Control flow operations
+constexpr uint8_t STATE_FLOW = 0x04;    // State flow operations
+
+} // namespace MaskContstants
+
+constexpr actionConstant SET_RELATIVE_LENGTH =
+    MaskConstants::SET_MASK &
+    MaskConstants::LENGTH & 0x0;
+
+constexpr actionConstant SET_ABSOLUTE_LENGTH = 
+    MaskConstants::SET_MASK &
+    MaskConstants::LENGTH & 0x1;
+
+constexpr actionConstant GET_TARGET_LENGTH = 
+    MaskConstants::GET_MASK &
+    MaskConstants::LENGTH & 0x2;
+    
+constexpr actionConstant GET_CURRENT_LENGTH = 
+    MaskConstants::GET_MASK &
+    MaskConstants::LENGTH & 0x3;
+
+constexpr actionConstant SET_VELOCITY = 
+    MaskConstants::SET_MASK &
+    MaskConstants::VELOCITY & 0x0;
+
+constexpr actionConstant GET_TARGET_VELOCITY = 
+    MaskConstants::GET_MASK &
+    MaskConstants::VELOCITY & 0x1;
+
+constexpr actionConstant GET_CURRENT_VELOCITY = 
+    MaskConstants::GET_MASK &
+    MaskConstants::VELOCITY & 0x2;
+
+constexpr actionConstant SET_HOME_MAX = 
+    MaskConstants::SET_MASK &
+    MaskConstants::HOMING & 0x0;
+
+constexpr actionConstant SET_HOME_MIN = 
+    MaskConstants::SET_MASK &
+    MaskConstants::HOMING & 0x1;
+
+constexpr actionConstant GET_HOMED = 
+    MaskConstants::GET_MASK &
+    MaskConstants::HOMING & 0x2;
+
+constexpr actionConstant SET_CONTROL = 
+    MaskConstants::SET_MASK &
+    MaskConstants::CONTROL_FLOW & 0x0;
+
+constexpr actionConstant GET_CONTROL = 
+    MaskConstants::GET_MASK &
+    MaskConstants::CONTROL_FLOW & 0x1;
+
+constexpr actionConstant SET_SPEED_PID = 
+    MaskConstants::SET_MASK &
+    MaskConstants::STATE_FLOW & 0x0;
+
+constexpr actionConstant SET_LENGTH_PID = 
+    MaskConstants::SET_MASK &
+    MaskConstants::STATE_FLOW & 0x1;
+
+constexpr actionConstant GET_SPEED_PID = 
+    MaskConstants::GET_MASK &
+    MaskConstants::STATE_FLOW & 0x2;
+
+constexpr actionConstant GET_LENGTH_PID = 
+    MaskConstants::GET_MASK &
+    MaskConstants::STATE_FLOW & 0x3;
+
+} // namespace ActuatorConstants
+
 namespace WatchdogConstants {
 constexpr uint16_t MAINTAIN_SLEEP_TIME = 50;  // The time to sleep between maintainState
                                               // loops, in milliseconds for the ROS nodes.

e61fea7712d49306b26ea3f76b70f07ac7003795
Jon <jar335@uakron.edu>
2025-04-17 00:48:54 +0000

    Call Macro Functions

--- ROS/src/ODrive.cpp
+++ ROS/src/ODrive.cpp
@@ -252,112 +252,6 @@ void ODriveModule::responseCallback(const roi_ros::msg::SerializedPacket respons
     // this->debugLog("Response handled");
 }
 
-void ODriveModule::publishPowerMessage() {
-    // Publish the power message, voltage and current
-    auto message = roi_ros::msg::ODrivePower();
-    message.voltage = _busVoltage;
-    message.current = _current;
-    this->_power_publisher_->publish(message);
-}
-
-void ODriveModule::publishStateMessage() {
-    // Publish the state message, position and velocity
-    auto message = roi_ros::msg::ODriveState();
-    message.position = _position;
-    message.velocity = _velocity;
-    this->_state_publisher_->publish(message);
-}
-
-void ODriveModule::publishTemperatureMessage() {
-    // Publish the temperature message, motor and fet
-    auto message = roi_ros::msg::ODriveTemperature();
-    message.motor_temp = _motorTemperature;
-    message.fet_temp = _fetTemperature;
-    this->_temperature_publisher_->publish(message);
-}
-
-void ODriveModule::gotoPositionServiceHandler(
-    const roi_ros::srv::ODriveGotoPosition::Request::SharedPtr request,
-    roi_ros::srv::ODriveGotoPosition::Response::SharedPtr response) {
-    // Handle the goto position service request
-    this->debugLog("Received goto position service request");
-
-    if (!this->validateVelTorque(request->velocity_feedforward, request->torque_feedforward)) {
-        this->debugLog("Invalid velocity or torque feedforward");
-        response->success = false;
-        return;
-    }
-
-    this->sendGotoPositionPacket(request->position, request->velocity_feedforward,
-                                 request->torque_feedforward);
-
-    // Respond to the service request
-    response->success = !_healthData._module_error;  // if there is an error, success is false, we
-                                                     // may have not done the request
-    this->debugLog("Goto position service request handled");
-}
-
-void ODriveModule::gotoRelativePositionServiceHandler(
-    const roi_ros::srv::ODriveGotoRelativePosition::Request::SharedPtr request,
-    roi_ros::srv::ODriveGotoRelativePosition::Response::SharedPtr response) {
-    // Handle the goto position service request
-    this->debugLog("Received goto position service request");
-
-    if (!this->validateVelTorque(request->velocity_feedforward, request->torque_feedforward)) {
-        this->debugLog("Invalid velocity or torque feedforward");
-        response->success = false;
-        return;
-    }
-
-    this->sendGotoRelativePositionPacket(request->position, request->velocity_feedforward,
-                                         request->torque_feedforward);
-
-    // Respond to the service request
-    response->success = !_healthData._module_error;  // if there is an error, success is false, we
-                                                     // may have not done the request
-    this->debugLog("Goto position service request handled");
-}
-
-void ODriveModule::setTorqueServiceHandler(
-    const roi_ros::srv::ODriveSetTorque::Request::SharedPtr request,
-    roi_ros::srv::ODriveSetTorque::Response::SharedPtr response) {
-    // Handle the set torque service request
-    this->debugLog("Received set torque service request");
-
-    if (!this->validateVelTorque(0, request->torque)) {
-        this->debugLog("Invalid velocity or torque feedforward");
-        response->success = false;
-        return;
-    }
-
-    this->sendSetTorquePacket(request->torque);
-
-    // Respond to the service request
-    response->success = !_healthData._module_error;
-
-    this->debugLog("Set torque service request handled");
-}
-
-void ODriveModule::setVelocityServiceHandler(
-    const roi_ros::srv::ODriveSetVelocity::Request::SharedPtr request,
-    roi_ros::srv::ODriveSetVelocity::Response::SharedPtr response) {
-    // Handle the set velocity service request
-    // this->debugLog("Received set velocity service request");
-
-    if (!this->validateVelTorque(request->velocity, request->torque_feedforward)) {
-        this->debugLog("Invalid velocity or torque feedforward");
-        response->success = false;
-        return;
-    }
-
-    this->sendSetVelocityPacket(request->velocity, request->torque_feedforward);
-
-    // Respond to the service request
-    response->success = !_healthData._module_error;
-
-    // this->debugLog("Set velocity service request handled");
-}
-
 void ODriveModule::sendGotoPositionPacket(float position, float velocity_feedforward,
                                           float torque_feedforward) {
     // Set the odrive to position mode if needed to complete request
@@ -527,199 +421,6 @@ void ODriveModule::sendSetVelocityPacket(float velocity, float torque_feedforwar
     this->sendGeneralPacket(packet);
 }
 
-rclcpp_action::GoalResponse ODriveModule::gotoPositionGoalHandler(
-    const rclcpp_action::GoalUUID &uuid,
-    std::shared_ptr<const roi_ros::action::ODriveGotoPosition::Goal> goal) {
-    this->debugLog("Received goto position action goal request");
-
-    if (!this->validateVelTorque(goal->velocity_feedforward, goal->torque_feedforward)) {
-        this->debugLog("Invalid velocity or torque feedforward");
-        return rclcpp_action::GoalResponse::REJECT;
-    }
-    if (this->_healthData._module_error) {
-        this->debugLog("Module error. Rejecting goal");
-        return rclcpp_action::GoalResponse::REJECT;
-    }
-
-    (void)uuid;  // suppress unused variable warning
-    (void)goal;  // suppress unused variable warning
-
-    return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;  // accept and execute any goal. we're
-                                                             // not picky
-}
-
-void ODriveModule::gotoPositionAcceptedHandler(
-    const std::shared_ptr<rclcpp_action::ServerGoalHandle<roi_ros::action::ODriveGotoPosition>>
-        goalHandle) {
-    this->debugLog(
-        "Received goto position action accepted request. Spinning up execution monitor thread");
-
-    // initiate the movement asap. don't wait for the thread to start
-    this->sendGotoPositionPacket(
-        goalHandle->get_goal()->position, goalHandle->get_goal()->velocity_feedforward,
-        goalHandle->get_goal()->torque_feedforward);  // send the goal to the module
-
-    std::thread(&ODriveModule::gotoPositionExecuteHandler, this, goalHandle)
-        .detach();  // spin up the execution thread
-}
-
-rclcpp_action::CancelResponse ODriveModule::gotoPositionCancelHandler(
-    const std::shared_ptr<rclcpp_action::ServerGoalHandle<roi_ros::action::ODriveGotoPosition>>
-        goalHandle) {
-    this->debugLog("Received goto position action cancel request");
-
-    this->sendSetVelocityPacket(0, 0);  // stop the motor
-
-    (void)goalHandle;  // ig remove the goal handle
-
-    return rclcpp_action::CancelResponse::ACCEPT;  // accept any cancel request
-}
-
-void ODriveModule::gotoPositionExecuteHandler(
-    const std::shared_ptr<rclcpp_action::ServerGoalHandle<roi_ros::action::ODriveGotoPosition>>
-        goalHandle) {
-    this->debugLog("Executing goto position action goal loop");
-    const auto goal = goalHandle->get_goal();  // get the goal
-    auto feedback =
-        std::make_shared<roi_ros::action::ODriveGotoPosition::Feedback>();  // create a feedback
-                                                                            // object
-    auto result =
-        std::make_shared<roi_ros::action::ODriveGotoPosition::Result>();  // create a result object
-
-    // loop until the goal is complete
-    while (rclcpp::ok() &&
-           (this->_velocity > 0.5 ||
-            this->_position == goal->position)) {  // check if the node is still running and the
-                                                   // velocity is above 0.5 rev/s
-        // update the feedback
-        feedback->current_position = this->_position;
-        feedback->current_velocity = this->_velocity;
-        goalHandle->publish_feedback(feedback);  // publish the feedback
-
-        // sleep for 75ms
-        std::this_thread::sleep_for(
-            std::chrono::milliseconds(75));  // sleep for 75ms we don't need to be that fast. This
-                                             // ensures the feedback is read between publishes
-
-        // check if cancel has been requested
-        if (goalHandle->is_canceling() ||
-            this->_healthData._module_error) {  // check if the goal has been canceled or there is
-                                                // an error. If so, cancel the goal
-            result->success = false;            // set the result to false
-            goalHandle->canceled(result);       // cancel the goal (this calls the cancel callback)
-            this->debugLog("Goto position action goal canceled");
-            return;  // exit the loop
-        }
-    }
-
-    // if the loop exits, the goal is complete
-    result->success = true;       // set the result to true
-    goalHandle->succeed(result);  // succeed the goal
-    this->debugLog("Goto position action goal succeeded");
-}
-
-rclcpp_action::GoalResponse ODriveModule::gotoRelativePositionGoalHandler(
-    const rclcpp_action::GoalUUID &uuid,
-    std::shared_ptr<const roi_ros::action::ODriveGotoRelativePosition::Goal> goal) {
-    this->debugLog("Received goto relative position action goal request");
-
-    if (!this->validateVelTorque(goal->velocity_feedforward, goal->torque_feedforward)) {
-        this->debugLog("Invalid velocity or torque feedforward");
-        return rclcpp_action::GoalResponse::REJECT;
-    }
-    if (this->_healthData._module_error) {
-        this->debugLog("Module error. Rejecting goal");
-        return rclcpp_action::GoalResponse::REJECT;
-    }
-
-    (void)uuid;  // suppress unused warning
-    (void)goal;  // suppress unused warning
-
-    return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;  // accept and execute any goal. we're
-                                                             // not picky
-}
-
-void ODriveModule::gotoRelativePositionAcceptedHandler(
-    const std::shared_ptr<
-        rclcpp_action::ServerGoalHandle<roi_ros::action::ODriveGotoRelativePosition>>
-        goalHandle) {
-    this->debugLog(
-        "Received goto relative position action accepted request. Spinning up execution monitor "
-        "thread");
-
-    this->_relativeStartPosition =
-        this->_position;  // store the current position as the start position
-
-    // initiate the movement asap. don't wait for the thread to start
-    this->sendGotoRelativePositionPacket(
-        goalHandle->get_goal()->position, goalHandle->get_goal()->velocity_feedforward,
-        goalHandle->get_goal()->torque_feedforward);  // send the goal to the module
-
-    std::thread(&ODriveModule::gotoRelativePositionExecuteHandler, this, goalHandle)
-        .detach();  // spin up the execution thread
-}
-
-rclcpp_action::CancelResponse ODriveModule::gotoRelativePositionCancelHandler(
-    const std::shared_ptr<
-        rclcpp_action::ServerGoalHandle<roi_ros::action::ODriveGotoRelativePosition>>
-        goalHandle) {
-    this->debugLog("Received goto relative position action cancel request");
-
-    this->sendSetVelocityPacket(0, 0);  // stop the motor
-
-    (void)goalHandle;  // ig remove the goal handle
-
-    return rclcpp_action::CancelResponse::ACCEPT;  // accept any cancel request
-}
-
-void ODriveModule::gotoRelativePositionExecuteHandler(
-    const std::shared_ptr<
-        rclcpp_action::ServerGoalHandle<roi_ros::action::ODriveGotoRelativePosition>>
-        goalHandle) {
-    this->debugLog("Executing goto relative position action goal loop");
-    const auto goal = goalHandle->get_goal();  // get the goal
-    auto feedback =
-        std::make_shared<roi_ros::action::ODriveGotoRelativePosition::Feedback>();  // create a
-                                                                                    // feedback
-                                                                                    // object
-    auto result =
-        std::make_shared<roi_ros::action::ODriveGotoRelativePosition::Result>();  // create a result
-                                                                                  // object
-
-    // loop until the goal is complete
-    while (rclcpp::ok() &&
-           (this->_velocity > 0.5 ||
-            abs(goal->position - this->_position -
-                this->_relativeStartPosition))) {  // check if the node is still running and the
-        // velocity is above 0.5 rev/s or the position is not reached
-        // update the feedback
-        feedback->current_position =
-            this->_position - this->_relativeStartPosition;  // relative position
-        feedback->current_velocity = this->_velocity;
-        goalHandle->publish_feedback(feedback);  // publish the feedback
-
-        // sleep for 75ms
-        std::this_thread::sleep_for(
-            std::chrono::milliseconds(75));  // sleep for 75ms we don't need to be that fast. This
-                                             // ensures the feedback is read between publishes
-
-        // check if cancel has been requested
-        if (goalHandle->is_canceling() ||
-            this->_healthData._module_error) {  // check if the goal has been canceled or there is
-                                                // an error. If so, cancel the goal
-            result->success = false;            // set the result to false
-            goalHandle->canceled(result);       // cancel the goal (this calls the cancel callback)
-            this->debugLog("Goto relative position action goal canceled");
-            return;  // exit the loop
-        }
-    }
-
-    // if the loop exits, the goal is complete
-    result->success = true;       // set the result to true
-    goalHandle->succeed(result);  // succeed the goal
-    this->debugLog("Goto relative position action goal succeeded");
-}
-
 std::string ODriveModule::oDriveErrorToString(uint32_t errorCode) {
     switch (errorCode) {
         __oDriveErrorCase(ODRIVE_ERROR_BAD_CONFIG, "Bad Configuration, invalid settings values.")
@@ -759,6 +460,57 @@ bool ODriveModule::validateVelTorque(float velocity, float torque) {
     }
     return true;
 }
+
+//-------- MACRO METHODS ---------//
+
+__stateMessage(publishPowerMessage, ODrivePower, 
+                voltage, _busVoltage, current, _current, 
+                _power_publisher_)
+
+__stateMessage(publishStateMessage, ODriveState, 
+                position, _position, velocity, _velocity, 
+                _state_publisher_)
+
+__stateMessage(publishTemperatureMessage, ODriveTemperature, 
+                motor_temp, _motorTemperature, fet_temp, _fetTemperature, 
+                _temperature_publisher_)
+
+__gotoServiceHander(gotoPositionServiceHandler, ODriveGotoPosition, "goto position", 
+                    request->velocity_feedforward, request->torque_feedforward, 
+                    this->sendGotoPositionPacket(request->position, request->velocity_feedforward, request->torque_feedforward))
+
+__gotoServiceHander(gotoRelativePositionServiceHandler, ODriveGotoRelativePosition, "goto relative position",
+                    request->velocity_feedforward, request->torque_feedforward,
+                    this->sendGotoRelativePositionPacket(request->position, request->velocity_feedforward, request->torque_feedforward))
+
+__gotoServiceHander(setTorqueServiceHandler, ODriveSetTorque, "set torque",
+                    0, request->torque,
+                    this->sendSetTorquePacket(request->torque))
+
+__gotoServiceHander(setVelocityServiceHandler, ODriveSetVelocity, "set velocity",
+                    request->velocity, request->torque_feedforward,
+                    this->sendSetVelocityPacket(request->velocity, request->torque_feedforward))
+
+__goalHandler(gotoPositionGoalHandler, "position", ODriveGotoPosition)
+
+__positionAcceptHandler(gotoPositionAcceptedHandler, "position", ODriveGotoPosition, sendGotoPositionPacket, gotoPositionExecuteHandler)
+
+__positionCancelHandle(gotoPositionCancelHandler, "position", ODriveGotoPosition)
+
+__positionExecuteHandler(gotoPositionExecuteHandler, "position", ODriveGotoPosition, 
+                        this->_position == goal->position, this->_position)
+
+__goalHandler(gotoRelativePositionGoalHandler, "relative position", ODriveGotoRelativePosition)
+
+__positionAcceptHandler(gotoRelativePositionAcceptedHandler, "relative position", 
+                        ODriveGotoRelativePosition, sendGotoRelativePositionPacket, 
+                        gotoRelativePositionExecuteHandler)
+
+__positionCancelHandle(gotoRelativePositionCancelHandler, "relative position", ODriveGotoRelativePosition)
+
+__positionExecuteHandler(gotoRelativePositionExecuteHandler, "relative position", ODriveGotoRelativePosition, 
+                        abs(goal->position - this->_position - this->_relativeStartPosition), 
+                        this->_position - this->_relativeStartPosition)
 
 //-------- PUBLIC METHODS --------//
 
